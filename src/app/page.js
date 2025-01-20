5f01cfadef5584a94926b09afac931880796457f
elisdotenv <elisdotenv@gmail.com>
2025-01-20 23:52:23 +0300

    Initial Commit by dev IV

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
 import styles from './page.module.css';
 export default function Home() {
   return (
-    <div className={`w-screen h-screen flex flex-col items-center justify-center bg-black`}>
+    <div className={`w-screen h-screen flex flex-col items-center justify-center bg-black ${styles.gradientBackground}`}>
       <p className={`text-white font-bold text-[40px] ${styles.glow}`}>
-        uptown<span className={`text-[orange] font-bold text-[40px]`}>lobby</span>
+        uptown<span className={`text-[#ff5100] font-bold text-[40px]`}>lobby</span>
       </p>
     </div>
   );
