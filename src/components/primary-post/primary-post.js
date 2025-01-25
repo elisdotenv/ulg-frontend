a283ee7296315fbaf17906e8434107113708409a
elisdotenv <elisdotenv@gmail.com>
2025-01-25 23:43:33 +0300

    Resize the footer icons

--- src/components/primary-post/primary-post.js
+++ src/components/primary-post/primary-post.js
@@ -11,7 +11,7 @@ import { PiDotOutlineFill } from 'react-icons/pi';
 /*import { HiChatBubbleBottomCenterText } from 'react-icons/hi2';*/
 import { updatedTimeFn } from '@/utils/date/date-functions';
 import { truncateDescription, truncateTitle } from '@/utils/truncate/truncations';
-import { UpvoteIconPressed, UpvoteIcon } from '@/ui/svg-icons/svg-icons';
+import { UpvoteIconPressed, UpvoteIcon, BookmarkIcon, ChatIcon } from '@/ui/svg-icons/svg-icons';
 
 export default function PrimaryPost({
   gotohref,
@@ -174,7 +174,7 @@ export default function PrimaryPost({
               {/* () ---> Handle Chat Functionality */}
               <span className={`flex items-center`}>
                 <span className={`${styles.UpvoteDownvote} ml-2`}>
-                  <svg
+                  {/* <svg
                     width='1em'
                     height='1em'
                     viewBox='0 0 24 24'
@@ -184,7 +184,8 @@ export default function PrimaryPost({
                       d='M8.084 3.217a35.447 35.447 0 017.05-.078l.782.078.279.031c1.089.121 1.885.372 2.606.828a4.516 4.516 0 011.664 1.86c.336.69.5 1.423.53 2.361l.005.321v3.975a4.493 4.493 0 01-3.545 4.392l-.207.04-2.089.346-2.86 2.992-.147.135c-.986.789-2.399.623-3.205-.324-.532-.625-.616-1.34-.51-2.29l.029-.224.038-.254.033-.187-1.332-.189a5.011 5.011 0 01-1.677-.55l-.253-.146-.243-.16a4.777 4.777 0 01-1.491-1.721 4.935 4.935 0 01-.532-1.972l-.009-.3V8.618c0-1.096.162-1.915.535-2.683.375-.77.94-1.4 1.664-1.859.649-.41 1.359-.655 2.288-.788l.318-.04.28-.031zm7.666 1.491a33.948 33.948 0 00-6.752-.075l-.748.075-.28.031c-.915.102-1.481.297-1.97.606a3.016 3.016 0 00-1.116 1.247c-.228.468-.357.989-.38 1.76l-.004.266v3.563c0 .577.134 1.116.375 1.587.242.471.592.874 1.024 1.18.37.263.801.453 1.276.554l.242.043 1.98.283c.339.048.457.096.575.175.119.078.262.187.27.386l-.002.024-.013.08-.164.741-.064.333c-.111.63-.167 1.332.09 1.634.263.309.7.39 1.037.187l.089-.062 2.998-3.135.13-.101.092-.063.077-.04.08-.03.035-.01.087-.02L17 15.545a2.993 2.993 0 002.495-2.77l.005-.182V8.618c0-.921-.13-1.506-.384-2.026A3.016 3.016 0 0018 5.345c-.44-.278-.943-.464-1.706-.572l-.265-.034-.279-.03zm-.55 6.294l.093.005c.398.044.707.36.707.746 0 .38-.301.693-.691.743l-.109.007H8.8l-.093-.005c-.398-.044-.707-.36-.707-.745 0-.38.301-.694.691-.744l.109-.007h6.4zm0-3.5l.093.004c.398.044.707.36.707.746 0 .38-.301.693-.691.743l-.109.007H8.8l-.093-.005C8.309 8.953 8 8.637 8 8.252c0-.38.301-.694.691-.744l.109-.007h6.4z'
                       fill='currentcolor'
                       fillRule='evenodd'></path>
-                  </svg>
+                  </svg>*/}
+                  <ChatIcon />
                 </span>
               </span>
             </div>
@@ -194,8 +195,8 @@ export default function PrimaryPost({
               onClick={() => setBookmark()}
               className={`bg-[#a8b3cf0b] flex items-center justify-center px-[12px] py-[16px] rounded-[6px] h-9 leading-[1.125rem]`}>
               {isBookMarked ? (
-                <span className={`text-[30px] text-[#a8b3cf]`}>
-                  <svg
+                <span className={`text-[34px] text-[#a8b3cf]`}>
+                  {/*  <svg
                     width='1em'
                     height='1em'
                     viewBox='0 0 24 24'
@@ -205,11 +206,12 @@ export default function PrimaryPost({
                       d='M15.874 3H8.126a3.357 3.357 0 00-3.35 3.152l-.772 12.77c-.028.459.106.915.38 1.286l.101.125c.666.764 1.818.9 2.647.287L12 17.023l4.868 3.597a1.964 1.964 0 003.128-1.7l-.771-12.767A3.358 3.358 0 0015.874 3zm0 1.5c.981 0 1.794.764 1.854 1.744l.771 12.768a.464.464 0 01-.74.402l-5.207-3.848a.929.929 0 00-1.104 0L6.24 19.414a.464.464 0 01-.74-.402l.773-12.768c.06-.98.872-1.744 1.853-1.744h7.748z'
                       fill='currentcolor'
                       fillRule='evenodd'></path>
-                  </svg>
+                  </svg>*/}
+                  <BookmarkIcon />
                 </span>
               ) : (
-                <span className={`text-[30px] text-[#a8b3cf]`}>
-                  <svg
+                <span className={`text-[34px] text-[#a8b3cf]`}>
+                  {/*   <svg
                     width='1em'
                     height='1em'
                     viewBox='0 0 24 24'
@@ -219,7 +221,8 @@ export default function PrimaryPost({
                       d='M15.874 3H8.126a3.357 3.357 0 00-3.35 3.152l-.772 12.77c-.028.459.106.915.38 1.286l.101.125c.666.764 1.818.9 2.647.287L12 17.023l4.868 3.597a1.964 1.964 0 003.128-1.7l-.771-12.767A3.358 3.358 0 0015.874 3zm0 1.5c.981 0 1.794.764 1.854 1.744l.771 12.768a.464.464 0 01-.74.402l-5.207-3.848a.929.929 0 00-1.104 0L6.24 19.414a.464.464 0 01-.74-.402l.773-12.768c.06-.98.872-1.744 1.853-1.744h7.748z'
                       fill='currentcolor'
                       fillRule='evenodd'></path>
-                  </svg>
+                  </svg>*/}
+                  <BookmarkIcon />
                 </span>
               )}
             </span>
